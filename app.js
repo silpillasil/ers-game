5537d92d9e3dccaa992b2953eb9be7d3d4be39a5
Aishani Sil <aishani.sil@berkeley.edu>
2020-05-22 00:27:46 -0700

    changed card names

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var ALL_CARDS, P0_CARDS, P1_CARDS;
 var TVAL_D;
-var bottom_card, top_card, recent_cards;
+var bottom_card, top_card, recent_cards, queue_card;
+var gamePlaying;
 
 TVAL_D = {
     'Ace': 1,
@@ -21,23 +22,22 @@ TVAL_D = {
 };
 
 recent_cards = [];
-top_card, bottom_card = null, null;
+top_card, bottom_card, queue_card = null, null, null;
+// queue_card is the card that is about to be put down on the top of stack.
+gamePlaying = false;
 
 class Card {
     constructor(suit, rank) {
         this.suit = suit;
         this.rank = rank;
-        this.pngName = (suit + '_' + rank).toString();
+        this.pngName = (rank + '_' + suit).toString();
     }
-
     toString() {
         return this.pngName;
     }
-
     toImgString() {
         return "PNG/" + this.pngName + ".png";
     }
-
     /** returns whether two cards qualify for an add10 SLAP. */
     static add10(upper, under) {
         if (TVAL_D[upper.rank] == null || TVAL_D[lower.rank] == null) {
@@ -48,12 +48,10 @@ class Card {
         }
         return false;
     }
-
     /** returns whether two cards qualify for a top-bottom SLAP. */
     static tpbm() {
         return top_card.rank == bottom_card.rank;
     }
-
     /** returns whether two cards qualify for a marriage or divorce. */
     static love(upper, under) {
         var pair = [upper.rank, under.rank];
@@ -89,6 +87,50 @@ function checkSlappable() {
     console.log("hi");
 }
 
+/** credits to https://stackoverflow.com/questions/
+2450954/how-to-randomize-shuffle-a-javascript-array
+*/
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  console.log("shuffle func " + randomIndex);
+  return array;
+}
+
+function printCardArr(array) {
+    var stri = "";
+    for (var i = 0; i < array.length; i++) {
+        stri += array[i].toString();
+        stri += " ";
+        if (i % 5 == 4) {
+            stri += "\n";
+        }
+    }
+    return stri;
+}
+
+function playCard(playerList) {
+    queue_card = playerList.pop();
+    last_top = top_card;
+    top_card = queue_card;
+    var image = document.querySelector('.currcard');
+    image.src = top_card.toImgString();
+}
+
+
+
 //////////
 
 ALL_CARDS = []; P0_CARDS = []; P1_CARDS = [];
@@ -118,20 +160,37 @@ document.addEventListener('DOMContentLoaded', function () { // why do you need t
     document.querySelector('#deal').addEventListener('click', function() {
         // DEAL function
         var d = 1;
-        for (var c = 0; c < ALL_CARDS.length; c++) {
+        var ALL_SHUFFLE = shuffle(ALL_CARDS);
+        for (var c = 0; c < ALL_SHUFFLE.length; c++) {
             if (d == 1) {
-                P1_CARDS.push(ALL_CARDS[c]);
+                P1_CARDS.push(ALL_SHUFFLE[c]);
             } else {
-                P0_CARDS.push(ALL_CARDS[c]);
+                P0_CARDS.push(ALL_SHUFFLE[c]);
             }
             d = 1-d;
         }
 
-        console.log([P0_CARDS.length, P1_CARDS.length]);
+        P0_CARDS = shuffle(P0_CARDS);
+        P1_CARDS = shuffle(P1_CARDS);
+
+        console.log("HERE");
+        console.log([printCardArr(P0_CARDS), printCardArr(P1_CARDS)]);
         console.log(ALL_CARDS.length);
 
-
     });
+
+    document.querySelector('#begin').addEventListener('click', function() {
+        if (!gamePlaying) {
+            gamePlaying = true;
+            document.querySelector('#begin').textContent = "quit game";
+        } else {
+            // TODO: code for confirming restart
+            gamePlaying = false;
+            document.querySelector('#begin').textContent = "start game";
+        }
+    });
+
+
 
 
 });
